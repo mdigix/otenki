b18f8e04de99b7483a69e8831c4baed79c6ffc9e
mdigix <grouses_joiner_0i@icloud.com>
2025-04-22 13:08:27 +0900

    Initial Commit

---
+++ otenki/otenkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  otenkiApp.swift
+//  otenki
+//
+//  Created by mdigix on 2025/04/22.
+//
+
+import SwiftUI
+
+@main
+struct otenkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ otenkiTests/otenkiTests.swift
@@ -0,0 +1,17 @@
+//
+//  otenkiTests.swift
+//  otenkiTests
+//
+//  Created by mdigix on 2025/04/22.
+//
+
+import Testing
+@testable import otenki
+
+struct otenkiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
